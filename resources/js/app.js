0f3a8db71e9a1c630d7f9972130f521a205f3441
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-06-23 09:47:07 -0400

    - Create Home and Task Component - Install Vue Router - Configure Vue Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,41 @@
-require('./bootstrap');
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require("./bootstrap");
+
+window.Vue = require("vue");
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+// import the Vue Component
+import Home from "./components/HomeComponent";
+import Task from "./components/TaskComponent";
+
+// Define routes
+const routes = [
+    {
+        path: "/",
+        component: Home
+    },
+    {
+        path: "/tasks",
+        component: Task
+    }
+];
+
+// Instantiate the routes
+const router = new VueRouter({ routes });
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: "#app",
+    router: router
+});
